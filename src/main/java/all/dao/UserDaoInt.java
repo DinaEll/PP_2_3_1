91639f4d7288ae289953da2a3b184dcf0b8a8120
Dina Pupina <goncharova.hope@gmail.com>
2022-10-23 17:52:12 +0300

    231. Есть дао и сервис, работает с SessionFactory, дао интерфейс не подключен

---
+++ src/main/java/all/dao/UserDaoInt.java
@@ -0,0 +1,15 @@
+package all.dao;
+
+import all.models.User;
+
+import java.util.List;
+
+public interface UserDaoInt {
+
+    List<User> index();
+    User show(int id);
+    void save(User user);
+    void update(int id, User updatedUser);
+    void delete(int id);
+
+}
---
+++ src/main/java/all/service/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package all.service;
+
+import all.dao.UserDao;
+import all.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDaoInt;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDaoInt) {
+        this.userDaoInt = userDaoInt;
+    }
+
+    @Transactional
+    @Override
+    public List<User> index() {
+        return this.userDaoInt.index();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public User show(int id) {
+        return this.userDaoInt.show(id);
+    }
+
+    @Transactional
+    @Override
+    public void save(User user) {
+        userDaoInt.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void update(int id, User updatedUser) {
+        userDaoInt.update(id, updatedUser);
+    }
+
+    @Transactional
+    @Override
+    public void delete(int id) {
+        this.userDaoInt.delete(id);
+    }
+
+}
