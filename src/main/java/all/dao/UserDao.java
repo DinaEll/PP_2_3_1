aa5c7350fe9f75e7f28de99b077a4bdc81deb234
Dina Pupina <goncharova.hope@gmail.com>
2022-10-24 22:50:11 +0300

    EntityManager

--- src/main/java/all/dao/UserDao.java
+++ src/main/java/all/dao/UserDao.java
@@ -17,27 +17,22 @@ public class UserDao {
     public UserDao() {
     }
 
-    @Transactional(readOnly = true)
     public List<User> index() {
         return entityManager.createQuery("select p from User p", User.class).getResultList();
     }
 
-    @Transactional(readOnly = true)
     public User show(int id) {
         return entityManager.find(User.class, id);
     }
 
-    @Transactional
     public void save(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional
     public void update(User updatedUser) {
         entityManager.merge(updatedUser);
     }
 
-    @Transactional
     public void delete(int id) {
        User user = entityManager.find(User.class, id);
         entityManager.remove(user);
